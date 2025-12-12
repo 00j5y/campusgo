46a72fc9aa63440316972b5b45bd5b962b38758f
cl2m5nt <clembgr76@gmail.com>
2025-12-12 17:06:36 +0100

    Ajout }

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,6 +65,7 @@ class User extends Authenticatable
     public function reservations()
     {
         return $this->belongsToMany(Trajet::class, 'reserver', 'id_utilisateur', 'id_trajet');
+    }
 
     
     public function vehicules() {
