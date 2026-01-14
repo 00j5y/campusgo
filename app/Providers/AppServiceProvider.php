31820e8873b2e5cbfada9dfdb575ac2b7ef83d48
00j5y <spmt.jay@gmail.com>
2026-01-14 17:30:19 +0100

    {FEAT} [ADD] Ajout fichier deploiement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('fr');
-        if($this->app->environment('production')) {
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
     }
