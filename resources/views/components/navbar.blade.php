8188e0366ea96b75cbdf3186454970b6c2f23308
00j5y <spmt.jay@gmail.com>
2026-01-14 18:28:48 +0100

    {FIX} Réaparition du bouton profil quand on est déconnecté dans la navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -29,14 +29,12 @@
                     </svg>
                     Mes Trajets
                 </a>
-                @auth
                 <a href="{{ route('profile.show') }}" class="flex items-center text-gris1 hover:text-vert-principale transition font-medium">
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                     </svg>
                     Profil
                 </a>
-                @endauth
 
                 @auth
                     @if(Auth::user()->est_admin == 1)
